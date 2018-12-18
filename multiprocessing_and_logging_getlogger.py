1d2698598b21bee3fca7f523bc285f0c412b0d02
goalkeeperfyc <664947387@qq.com>
2018-12-19 02:44:21 +0800

    update

--- multiprocessing_and_logging_getlogger.py
+++ multiprocessing_and_logging_getlogger.py
@@ -14,11 +14,25 @@ from multiprocessing import Pool
 class Test_Pool():
     
     def __init__(self):
+        """
         logging.basicConfig(level=logging.DEBUG,  
                             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
                             datefmt='%Y-%m-%d %H:%M:%S',  
-                            filename='/users/fangyucheng/Documents/code/python_code/test_for_new_module/test_log',  
+                            filename='/users/fangyucheng/Documents/code//python_code/test_for_new_module/test_log',  
                             filemode='a') 
+        logging.getLogger()        
+        sh = logging.StreamHandler() 
+        """
+        logger = logging.getLogger()
+        fh = logging.FileHandler('/users/fangyucheng/Documents/code/python_code/test_for_new_module/test_log2',encoding='utf-8')
+        sh = logging.StreamHandler()                     
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter2 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s [line:%(lineno)d] : %(message)s')
+        fh.setFormatter(formatter)
+        sh.setFormatter(formatter2)
+        logger.addHandler(fh)
+        logger.addHandler(sh)
+
     
     
     def func1(self, a, b=5):
