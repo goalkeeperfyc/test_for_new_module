45f2aeb67b6733b436eff045866ca0fa54497f4c
goalkeeperfyc <664947387@qq.com>
2018-12-19 10:03:25 +0800

    update

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 09:47:01 2018
+
+@author: fangyucheng
+"""
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,  
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='/home/fangyucheng/python_code/test_for_new_module/test_log',
+                    #filename='/users/fangyucheng/Documents/code/python_code/test_for_new_module/test_log',
+                    filemode='a') 
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
--- multiprocessing_and_logging.py
+++ multiprocessing_and_logging.py
@@ -8,36 +8,37 @@ Created on Tue Dec 11 19:38:48 2018
 import os
 import time
 import datetime
-import logging
 from multiprocessing import Pool
+import test_for_new_module
+
+
 
 class Test_Pool():
     
     def __init__(self):
-        logging.basicConfig(level=logging.DEBUG,  
-                            format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
-                            datefmt='%Y-%m-%d %H:%M:%S',
-                            filename='/home/fangyucheng/python_code/test_for_new_module/test_log',
-                            #filename='/users/fangyucheng/Documents/code/python_code/test_for_new_module/test_log',
-                            filemode='a') 
-        console = logging.StreamHandler()
-        console.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-        console.setFormatter(formatter)
-        logging.getLogger('').addHandler(console)
+        pass
+#        logging.basicConfig(level=logging.DEBUG,  
+#                            format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
+#                            datefmt='%Y-%m-%d %H:%M:%S',
+#                            filename='/home/fangyucheng/python_code/test_for_new_module/test_log',
+#                            #filename='/users/fangyucheng/Documents/code/python_code/test_for_new_module/test_log',
+#                            filemode='a') 
+#        console = logging.StreamHandler()
+#        console.setLevel(logging.DEBUG)
+#        formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+#        console.setFormatter(formatter)
+#        logging.getLogger('').addHandler(console)
         
     def func1(self, a, b=5):
         pid = os.getpid()
         c = a + b
-        print(c)
-        logging.info('this is process %s with %s' % (pid, c))
+        test_for_new_module.logging.info('this is process %s with %s' % (pid, c))
         time.sleep(5)
-        print("this is process %s" % pid)
-        logging.info('this is process %s' % pid)
+        test_for_new_module.logging.info('this is process %s' % pid)
         
     def func2(self, c, d):
         pid = os.getpid()
-        logging.info('this is process %s, with input number %s %s' % (pid, c, d))
+        test_for_new_module.logging.info('this is process %s, with input number %s %s' % (pid, c, d))
 
 if __name__ == '__main__':
     test = Test_Pool()
