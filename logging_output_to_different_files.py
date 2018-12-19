1dd7e7d8e080971461cf1c4372aac29e16d46f8f
goalkeeperfyc <664947387@qq.com>
2018-12-19 16:36:58 +0800

    update

--- logging_output_to_different_files.py
+++ logging_output_to_different_files.py
@@ -20,11 +20,3 @@ def setup_logger(name, log_file, level=logging.INFO):
     logger.addHandler(handler)
 
     return logger
-
-# first file logger
-logger = setup_logger('first_logger', 'first_logfile.log')
-logger.info('This is just info message')
-
-# second file logger
-super_logger = setup_logger('second_logger', 'second_logfile.log')
-super_logger.error('This is an error message')
