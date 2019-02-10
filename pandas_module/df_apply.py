730e6f2617bcc3b7cef9290ef3b033bbfe426fb4
goalkeeperfyc <664947387@qq.com>
2019-02-10 14:40:52 -0500

    commit by code

--- pandas_module/df_apply.py
+++ pandas_module/df_apply.py
@@ -8,5 +8,23 @@ Email: 664947387@qq.com
 """
 
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame(list(range(1,20)))
+
+def function():
+    if df[0] > 5:
+        return 0
+    else:
+        return 1
+
+#new_df = df[0].apply(lambda x: function())
+new_df2 = df.apply(np.sum)
+#
+#if df[0] > 5:
+#    df['new_column'] = 1
+#else:
+#    df['new_column'] = 0
+
+df['bool'] = df[0] > 5
+df['bool_to_binary'] = df['bool'].astype('int')
