f65ac6a4af57fe6388545b76fa3ea1b599289781
goalkeeperfyc <goalkeeperyucheng@gmail.com>
2020-04-04 20:32:49 -0400

    update

--- Integral.py
+++ Integral.py
@@ -1,4 +1,6 @@
 from scipy import integrate
+import numpy as np
+import math
 
 
 def f(x, a, b):
@@ -7,5 +9,15 @@ def f(x, a, b):
 
 v, err = integrate.quad(f, 1, 2, args=(-1, 1))
 
-print(v) # v is the result
-print(err) # err is the absolute error
+print(v)  # v is the result
+print(err)  # err is the absolute error
+
+
+def normal_distribution(x, mean, std):
+    return 1 / (std * math.sqrt(2 * math.pi)) * math.exp(-0.5 * ((x - mean) / std) ** 2)
+
+
+v2, err2 = integrate.quad(normal_distribution, -np.inf, 0, args=(0, 1))
+
+print(v2)
+
