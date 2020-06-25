08ef1836b6a1c2cd4e2240ec999c7b143b2ab727
Yucheng Fang <32383811+goalkeeperfyc@users.noreply.github.com>
2020-06-25 13:14:24 -0400

    create logging setting

---
+++ logging_module/logging_setting.py
@@ -0,0 +1,18 @@
+import logging
+
+### set up logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='linear_model.log',
+                    filemode='a')
+
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger().addHandler(console)
